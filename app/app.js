8d202c459647cb5bc98fa7a8b2c84394f1af3c2d
HienD <hdduong.at.wpi.edu@gmail.com>
2017-03-26 17:07:22 -0700

    routing to multiple view part 2

--- app/app.js
+++ app/app.js
@@ -2,12 +2,38 @@
  * Created by HienD on 3/26/2017.
  */
 (function() {
-    "use strict"
-    var app=angular.module("productManagement",
-                            [
-                                "commonService",
-                                "ui.router",
-                                "productResourceServiceMock"
-                            ]);
+    "use strict";
+
+    var app = angular.module("productManagement",
+                            ["commonService",
+                             "ui.router",
+                             "productResourceServiceMock"]);
+
+/*    var app = angular.module("productManagement",
+        ["commonService",
+            "ui.router"]);*/
+
+    app.config(["$stateProvider","$urlRouterProvider", function ($stateProvider,$urlRouterProvider) {
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state("home", {
+                url: "/",
+                templateUrl: "app/html/welcomeView.html"
+            })
+
+            .state("productList", {
+                url:"/products",
+                templateUrl: "app/html/productListView.html",
+                controller: "ProductListCtrl as vm"
+            })
+
+            .state("productEdit", {
+                url: "/products/edit/:productId",
+                templateUrl: "app/html/productEditView.html",
+                controller: "ProductEditCtrl as vm"
+            })
+        }]
+    );
 }());
 
